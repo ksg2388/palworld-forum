5d7319df166ca0924c83d561f8860d9a50cac555
ksg2388 <www.naver.com>
2025-02-03 15:40:15 +0900

    Fix: 기존 상태로 복귀

--- src/app/_components/editor/TuiEditor.tsx
+++ src/app/_components/editor/TuiEditor.tsx
@@ -3,23 +3,19 @@
 import dynamic from "next/dynamic";
 import { forwardRef } from "react";
 import "@toast-ui/editor/dist/toastui-editor.css";
-import { Editor } from "@toast-ui/react-editor";
+import { Editor as EditorType } from "@toast-ui/react-editor";
 // import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
 // import "tui-color-picker/dist/tui-color-picker.css";
 import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
 
-const EditorComponent = dynamic(
+const Editor = dynamic(
   () => import("@toast-ui/react-editor").then((mod) => mod.Editor),
   { ssr: false }
 );
 
-interface TuiEditorProps {
-  onImageUpload: (file: File) => Promise<string>;
-}
-
-const TuiEditor = forwardRef<Editor, TuiEditorProps>((props, ref) => {
+const TuiEditor = forwardRef<EditorType>((_, ref) => {
   return (
-    <EditorComponent
+    <Editor
       ref={ref}
       initialValue=""
       previewStyle="vertical"
@@ -27,7 +23,6 @@ const TuiEditor = forwardRef<Editor, TuiEditorProps>((props, ref) => {
       initialEditType="wysiwyg"
       useCommandShortcut={true}
       hideModeSwitch={true}
-      hooks={{ addImageBlobHook: props.onImageUpload }}
       // plugins={[colorSyntax]}
     />
   );

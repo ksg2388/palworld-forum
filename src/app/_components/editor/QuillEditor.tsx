96056ef049d44463af64d960333c5206df2ff3dc
ksg2388 <www.naver.com>
2025-02-04 15:40:55 +0900

    Fix: 글 수정 시 초기값 제대로 불러오도록 수정

--- src/app/_components/editor/QuillEditor.tsx
+++ src/app/_components/editor/QuillEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { forwardRef, useState } from "react";
+import { forwardRef, useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import 'react-quill-new/dist/quill.snow.css';
 
@@ -29,10 +29,11 @@ const formats = [
 ];
 
 const QuillEditor = forwardRef(({ initialValue = "" }: { initialValue?: string }, ref: any) => {
-  const [value, setValue] = useState<string>(initialValue);
+  const [value, setValue] = useState<string>("");
 
-  console.log("초기값 : ", initialValue);
-  console.log("현재값 : ", value);
+  useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
 
   // ref를 통해 외부에서 value에 접근할 수 있도록 설정
   if (ref) {

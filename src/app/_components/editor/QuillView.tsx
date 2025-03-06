0a90046f7ee2c8003658e330a120cb4db80619b4
ksg2388 <ksg2388@naver.com>
2025-03-06 17:56:25 +0900

    Fix: 뷰어 수정

--- src/app/_components/editor/QuillView.tsx
+++ src/app/_components/editor/QuillView.tsx
@@ -28,52 +28,6 @@ const QuillView = ({ content }: QuillViewProps) => {
             width: 100%;
             aspect-ratio: 16/9;
           }
-
-          /* Quill 기본 스타일 복원 */
-          .ql-editor {
-            padding: 12px 15px;
-            font-size: 1rem;
-            line-height: 1.5;
-          }
-
-          .ql-editor li {
-            list-style-type: none !important;
-          }
-          
-          .ql-editor p {
-            margin-bottom: 1em;
-          }
-          
-          .ql-editor h1, .ql-editor h2, .ql-editor h3 {
-            font-weight: bold;
-            margin-bottom: 0.5em;
-            margin-top: 1em;
-          }
-          
-          .ql-editor h1 {
-            font-size: 2em;
-          }
-          
-          .ql-editor h2 {
-            font-size: 1.5em;
-          }
-          
-          .ql-editor h3 {
-            font-size: 1.17em;
-          }
-          
-          .ql-editor ul, .ql-editor ol {
-            padding-left: 1.5em;
-            margin-bottom: 1em;
-          }
-          
-          .ql-editor ul > li {
-            list-style-type: disc;
-          }
-          
-          .ql-editor ol > li {
-            list-style-type: decimal;
-          }
         `}
       </style>
       <ReactQuillNew
